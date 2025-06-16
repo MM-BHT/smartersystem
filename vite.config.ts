bee047334c51fb70519dbcdadf7484ef3637c8ca
unknown <s83882@bht-berlin.de>
2025-06-16 20:42:20 +0200

    wt3

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/smartersystem",
+  base: "/smartersystem/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
