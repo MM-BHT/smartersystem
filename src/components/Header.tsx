026c2f2d09f9c9e110b1459f84ea65bf352325fe
unknown <s83882@bht-berlin.de>
2025-06-17 19:03:08 +0200

    xx1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,7 +21,7 @@ const Header: React.FC = () => {
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-2 text-blue-600 hover:text-blue-700 transition-colors">
-            <img src="/logo.png" alt="Smarter System Logo" className="h-8 w-8" />
+            <img src="logo.png" alt="Smarter System Logo" className="h-8 w-8" />
             <span className="font-bold text-xl">Smarter System</span>
           </Link>
 
